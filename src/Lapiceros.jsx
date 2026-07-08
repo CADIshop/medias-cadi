ba6889a3e84eb3a37a12275ed9b0cccf1b4453da
CADIshop <ventas@mediascadi.com>
2026-07-08 17:42:51 -0500

    Update Lapiceros.jsx

--- src/Lapiceros.jsx
+++ src/Lapiceros.jsx
@@ -420,13 +420,13 @@ export default function Lapiceros() {
           whileHover={{ y: -8 }}
           className="h-full w-full object-contain p-6 transition duration-500 hover:scale-110"
         >
-          <div className="mb-5 h-48 overflow-hidden rounded-[1.5rem] bg-slate-100">
-            <img
-              src={item.image}
-              alt={item.name}
-              className="h-full w-full object-cover transition duration-500 hover:scale-110"
-            />
-          </div>
+          <div className="mb-5 aspect-square overflow-hidden rounded-[1.5rem] bg-slate-100">
+  <img
+    src={item.image}
+    alt={item.name}
+    className="h-full w-full object-contain p-5 transition duration-500 hover:scale-110"
+  />
+</div>
 
           <h3 className="text-2xl font-black">
             {item.name}
@@ -458,13 +458,13 @@ export default function Lapiceros() {
           whileHover={{ y: -8 }}
           className="rounded-[2rem] border border-white bg-white p-5 shadow-lg shadow-slate-100"
         >
-          <div className="mb-5 h-72 overflow-hidden rounded-[1.5rem] bg-slate-100">
-            <img
-              src={item.image}
-              alt={item.name}
-              className="h-full w-full object-contain p-6 transition duration-500 hover:scale-110"
-            />
-          </div>
+          <div className="mb-5 aspect-square overflow-hidden rounded-[1.5rem] bg-slate-100">
+  <img
+    src={item.image}
+    alt={item.name}
+    className="h-full w-full object-contain p-5 transition duration-500 hover:scale-110"
+  />
+</div>
 
           <h3 className="text-2xl font-black">
             {item.name}
